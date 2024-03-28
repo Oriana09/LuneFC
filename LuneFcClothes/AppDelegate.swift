1a9a0153fcbaf4d4ba7d2c82402b58de0ab1669f
oriana09 <naircostancio@gmailcom>
2024-03-28 14:05:11 -0300

    Delete old assets

--- LuneFcClothes/AppDelegate.swift
+++ LuneFcClothes/AppDelegate.swift
@@ -10,46 +10,29 @@ import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-         
-        // Configurar la versión de la base de datos de Realm
-             let config = Realm.Configuration(
-                 // Incrementa el número de versión si cambias la estructura de tu modelo de datos
-                 schemaVersion: 1,
-                 migrationBlock: { migration, oldSchemaVersion in
-                     // No necesitas implementar un bloque de migración en este caso
-                 }
-             )
-             Realm.Configuration.defaultConfiguration = config
         
-        do {
-            let realm = try Realm()
-        } catch {
-            print("Error initialising new realm, \(error)")
-        }
-        
-        
+        self.configureRealm()
         
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
+        
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    private func configureRealm() {
+        let config = Realm.Configuration(
+            schemaVersion: 1,
+            migrationBlock: { migration, oldSchemaVersion in }
+        )
+   
+        Realm.Configuration.defaultConfiguration = config
+    }
 }
 
